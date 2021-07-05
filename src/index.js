86c690131ec68ca2604b8d614d0212a6585f5607
Jonah Skinner <jonah.lee.skinner@gmail.com>
2021-07-05 16:13:41 +1000

    1st commit

---
+++ src/index.js
@@ -0,0 +1,74 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/font-awesome/css/font-awesome.min.css";
+import "assets/scss/argon-design-system-react.scss?v1.1.0";
+
+import Index from "views/Index.js";
+import Landing from "views/examples/Landing.js";
+import Login from "views/examples/Login.js";
+import Profile from "views/examples/Profile.js";
+import Register from "views/examples/Register.js";
+import CareerWheel from  "views/examples/CareerWheel.js";
+import FindAWord from "views/examples/FindAWord.js";
+import FutureMeTree from "views/examples/FutureMeTree.js";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact render={props => <Index {...props} />} />
+      <Route
+        path="/landing-page"
+        exact
+        render={props => <Landing {...props} />}
+      />
+      <Route path="/login-page" exact render={props => <Login {...props} />} />
+      <Route
+        path="/profile-page"
+        exact
+        render={props => <Profile {...props} />}
+      />
+       <Route
+        path="/career-wheel-page"
+        exact
+        render={props => <CareerWheel {...props} />}
+      />
+       <Route
+        path="/FutureMeTree-page"
+        exact
+        render={props => <FutureMeTree {...props} />}
+      />
+       <Route
+        path="/Findaword-page"
+        exact
+        render={props => <FindAWord {...props} />}
+      />
+      <Route
+        path="/register-page"
+        exact
+        render={props => <Register {...props} />}
+      />
+      <Redirect to="/" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
